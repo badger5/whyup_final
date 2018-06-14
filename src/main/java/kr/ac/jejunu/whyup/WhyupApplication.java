737dd358fa71e7f1a0809ffcd035acfbfeb865c7
badger5 <dldhwn56@gmail.com>
2018-06-14 17:59:23 +0900

    first commit add front

---
+++ src/main/java/kr/ac/jejunu/whyup/WhyupApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.jejunu.whyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyupApplication.class, args);
+	}
+}
